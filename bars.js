7a5614b0db6e5a6844a14fb490e4b42c8849b860
Ayesha Zahid <ayeshazahid296@gmail.com>
2024-09-07 20:23:00 +0500

    navbar done

---
+++ bars.js
@@ -0,0 +1,13 @@
+document.querySelector('.bars').addEventListener('click', function () {
+    document.getElementById('side-menu').style.display = 'block'; // Make sure the menu is visible
+    setTimeout(() => {
+        document.getElementById('side-menu').classList.add('active');
+    }, 10);
+});
+
+document.querySelector('.close-btn').addEventListener('click', function () {
+    document.getElementById('side-menu').classList.remove('active');
+    setTimeout(() => {
+        document.getElementById('side-menu').style.display = 'none';
+    }, 300);
+});
